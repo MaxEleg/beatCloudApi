0d1feb4ecbba73292b81a71b37228ec8badc8ced
maxeleg <maximilien.elegbe@outlook.fr>
2018-06-24 19:27:09 +0200

    add upload

--- server/boot/3-upload-note.js
+++ server/boot/3-upload-note.js
@@ -7,25 +7,20 @@ module.exports = function upload(app) {
   var storage = multer.diskStorage({
     // destination
     destination: function(req, file, cb) {
-      cb(null, './public/uploads/');
+      cb(null, './uploads/sound');
     },
     filename: async function(req, file, cb) {
       try {
-        var Note = app.models.Note({name : file.originalname});
-        var newNote =  await (new Note()).save();
-        cb(null, file.originalname);
+        cb(null, req.originalname);
       } catch(ex){
-        console.err(ex);
+        console.log(ex);
         res.status(400).json({ msg: "Une erreur s'est produite"});
       }
     }
   });
   var upload = multer({storage: storage});
 
-  app.post('/uploads', upload.array('uploads[]', 1), function(req, res) {
+  app.post('/upload', upload.array('files[]', 27), function(req, res) {
     res.send(req.files);
-  });
-  app.get('/uploads', function(req, res) {
-    res.send('');
   });
 };
