9b723dc7e4bc1dba8f021a7f6ebc505938d6e173
maxeleg <maximilien.elegbe@outlook.fr>
2018-06-29 20:50:49 +0200

    add config db

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,9 +1,17 @@
 var config = require('../config');
 
+var url =  'mongodb://';
+if (config.mongodb.user.length > 0) {
+  url +=  config.mongodb.user + ':' + config.mongodb.password + '@';
+}
+url += config.mongodb.host;
+url += ':' + config.mongodb.port;
+url += '/' + config.mongodb.database;
+
 var mongodb = {
   host: config.mongodb.host,
   port: config.mongodb.port,
-  url: 'mongodb://esgi:esgi123123@ds235239.mlab.com:35239/beatcloud',
+  url: url,
   database: config.mongodb.database,
   password: config.mongodb.password,
   name: 'mongodb',
