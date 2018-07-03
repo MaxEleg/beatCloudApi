ad7fb914839e4a796c75ec4f1343ce78da554796
Elegbe Maximilien <maximilien.elegbe@outlook.fr>
2018-07-04 01:24:11 +0200

    init midlleware

--- controllers/middlewares/logged.js
+++ controllers/middlewares/logged.js
@@ -1,2 +1,14 @@
-module.exports = function({};
-                          })
+module.exports = function(app){
+  return function(req, res, next){
+
+    var accessToken = app.models.AccessToken;
+
+    if(!req.query.token){
+      res.status(400).json({msg: 'Token no provided'});
+      return;
+    }
+
+
+
+  };
+}
